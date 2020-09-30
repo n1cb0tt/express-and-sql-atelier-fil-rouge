5061d8a6e3d0fb3f711f548349ccab6337d5b059
n1cb0tt <nicbott@gmail.com>
2020-09-30 11:42:02 +0200

    conf

--- conf.js
+++ conf.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Mysql1977Mysql00!',
+  password: '',
   database: 'quotes'
 });
 
